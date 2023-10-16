5e098d65a1638b6c9eee335d0e174082f4223297
Kayla Harrison <kaylaharrison@kaylas-air.lan>
2023-10-15 22:18:08 -0400

    first commit - hello in console on submit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
+
+export default App;
